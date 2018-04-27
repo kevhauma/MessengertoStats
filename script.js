63eeb718ee9cf720d5b2c3c0d3f0202ce49ddc88
kevhauma <hauman.kevin@hotmail.com>
2018-04-27 18:49:14 +0200

    removed monhts.txt

--- script.js
+++ script.js
@@ -3,20 +3,9 @@ const _cliProgress = require('cli-progress')
 const readlineSync = require('readline-sync');
 let emojiRegex = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug
 let monthsArray = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sept", "oct", "nov", "dec"]
+let daysArray = ["mon", "tue", "wed", "thur", "fri", "sat", "sun"]
 let json = []
 let f = 0
-
-fs.readFile('./months.txt', 'utf8', function (err, data) { //read
-    let potMonthsArray = data.split(",")
-    for (let i = 0; i < potMonthsArray.length; i++)
-        potMonthsArray[i] = potMonthsArray[i].toLowerCase().trim()
-    if (potMonthsArray.length < 12) {
-        console.log("     months.txt contains less than 12 months\n     be sure to put a ',' between months\n    results will be incorrect")
-        exit()
-    } else monthsArray = potMonthsArray
-})
-
-
 
 console.log("if you crash on an invalid month, please change the month.txt to the appropriate language")
 
@@ -352,22 +341,7 @@ function exit() {
 }
 
 function getDaybyNumber(day) {
-    switch (day) {
-        case 1:
-            return 'mon'
-        case 2:
-            return 'tue'
-        case 3:
-            return 'wed'
-        case 4:
-            return 'thur'
-        case 5:
-            return 'fri'
-        case 6:
-            return 'sat'
-        case 0:
-            return 'sun'
-    }
+    return monthsArray[day]
 }
 
 function getMonthbyNumber(month) {
